97a554235478a62a7b0ba84460dcc570bbb13fae
Fabio Benedetti <fabyo82@outlook.com>
2019-12-20 20:06:27 +0100

    wip

--- server/mergeAudio.ts
+++ server/mergeAudio.ts
@@ -6,7 +6,12 @@ process.env.FFMPEG_PATH = ffmpegPath.path;
 const SILENCE = './audio/silence1.mp3';
 const BUTTON = './audio/button.mp3';
 const nullAudio = 'https://quizlet.com/null';
+ffmpeg(BUTTON).ffprobe(function(err, data) {
+  console.log('file2 metadata:');
+  console.dir(data);
+});
 export const mergeAudio = (source: string[], target: string, twice: boolean) => {
+  console.log('merging');
   source = source.map(i => (i === nullAudio ? SILENCE : i));
   return new Promise((resolve, reject) => {
     const f = ffmpeg();
@@ -37,13 +42,23 @@ export const mergeAudio = (source: string[], target: string, twice: boolean) => 
       //   console.log('Stderr output: ' + stderrLine);
       // })
       .on('error', function(err) {
-        console.log('An error occurred: ' + err.message);
+        console.log('An error occurred: ' + err.message + JSON.stringify(source));
         reject(err);
       })
-      .on('end', function() {
+      .on('end', function(data) {
+        console.log(data, 'data');
         // console.log('end');
         resolve();
       })
       .mergeToFile(target);
   });
 };
+
+mergeAudio([BUTTON, SILENCE], './tmp/test.mp3', false);
+const safeGet = (x,a1,a2) => {
+	
+}
+
+const y = {c:{a:{b:{C:3}}}};
+           
+const xt = safeGet(y, "a","s")
